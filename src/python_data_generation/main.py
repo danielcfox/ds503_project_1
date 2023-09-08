0e2c51d1af581c7c6fa7cbf49d6fd7a037b2506f
Minhhang <mgradetsky@wpi.edu>
2023-09-08 16:42:14 -0400

    added the draft of python program

---
+++ src/python_data_generation/main.py
@@ -0,0 +1,66 @@
+from faker import Faker
+import csv
+import random
+import string
+from my_faker import Hobby, Friendship, Access, MyTime
+import numpy as np
+import time
+
+# Initialize faker classes to generate random values.
+fake = Faker()
+hobby = Hobby()
+friendship = Friendship()
+access = Access()
+mytime = MyTime()
+
+
+# Generate FaceInPage dataset
+# -------------------------------------------------------------------------------------------------
+facein_ids = []
+
+start_time = time.time()
+
+with open('FaceInPage.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    # writer.writerow(["ID", "Name", "Nationality", "CountryCod", "Hobby"])  # add if want the column names to appear in the final csv file.
+    for i in range(1, 200001):
+        facein_ids.append(i)
+        writer.writerow([i, fake.name(), fake.country(), random.randint(1, 50), hobby.get_rand()])
+
+print(f"Datasets FaceInPage.csv generated successfully after {time.time() - start_time} seconds!")
+
+# Generate Associates dataset
+# -------------------------------------------------------------------------------------------------
+
+start_time = time.time()
+facein_ids = np.array(facein_ids)
+with open('Associates.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    friend_relations = set() # to keep track of equivalent symmetrical relationships
+
+    # writer.writerow(["FriendRel", "PersonA_ID", "PersonB_ID", "DateofFriendship", "Desc"])  # add if want the column names to appear in the final csv file.
+    for i in range(1, 20000001):
+        person_a_id = np.random.choice(facein_ids)
+        available_ids = facein_ids[facein_ids != person_a_id]
+        person_b_id = np.random.choice(available_ids)
+        if (person_a_id, person_b_id) not in friend_relations and (person_b_id, person_a_id) not in friend_relations:
+            friend_relations.add((person_a_id, person_b_id))
+            friend_relations.add((person_b_id, person_a_id)) # adding this due to the symmetrical relationship between a and b
+            writer.writerow([i, person_a_id, person_b_id, mytime.get_rand(), friendship.get_rand()]) 
+
+print(f"Datasets Associates.csv generated successfully after {time.time() - start_time} seconds!")
+
+# Generate AccessLogs dataset
+# -------------------------------------------------------------------------------------------------
+start_time = time.time()
+
+with open('AccessLogs.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    # writer.writerow(["AccessId", "ByWho", "WhatPage", "TypeOfAccess", "AccessTime"])  # add if want the column names to appear in the final csv file.
+    
+    for i in range(1, 10000001):
+        bywho = np.random.choice(facein_ids)
+        writer.writerow([i, bywho, random.randint(1, 200000), access.get_rand(), mytime.get_rand()])
+
+print(f"Datasets AccessLogs.csv generated successfully after {time.time() - start_time} seconds!")
+
---
+++ src/python_data_generation/my_faker.py
@@ -0,0 +1,49 @@
+import random
+import time
+
+class Hobby:
+    def __init__(self, name=None):
+        self.name = name
+        self.hobbies_types = ["Reading", "Writing", "Drawing", "Painting", "Photography", "Hiking", "Cycling", "Swimming", "Running", "Gaming", "Traveling", "Fishing", "Gardening", "Cooking", "Baking", "Knitting", "Camping", "Birdwatching", "Puzzles", "Dancing", "Yoga", "Meditation", "Playing Guitar", "Playing Piano", "Singing", "Scrapbooking", "Pottery", "Jewelry Making", "Skiing", "Snowboarding", "Surfing", "Skateboarding", "Sailing", "Scuba Diving", "Rock Climbing", "Archery", "Tennis", "Golf", "Basketball", "Baseball", "Soccer", "Football", "Rugby", "Volleyball", "Martial Arts", "Boxing", "Fencing", "Table Tennis", "Billiards", "Bowling", "Roller Skating", "Ice Skating", "Kite Flying", "Stargazing", "Astronomy", "Magic Tricks", "Cosplaying", "Beekeeping", "Wine Tasting", "Craft Beer Brewing", "Mixology", "Candle Making", "Soap Making", "Calligraphy", "Origami", "Chess", "Drone Flying", "Blogging", "Podcasting", "Vlogging", "Horseback Riding", "Canoeing", "Kayaking", "Wind Surfing", "Paragliding", "Skydiving", "Bungee Jumping", "Model Building", "Homebrewing", "Quilting", "Whittling", "Macrame", "Embroidery", "Crocheting", "Stamp Collecting", "Coin Collecting", "Metal Detecting", "Rafting", "Sudoku", "Crosswords", "Geocaching", "Urban Exploration", "Aquarium Keeping", "Tarot Reading", "Beatboxing", "Radio Control Cars", "Parkour", "Board Games", "Digital Art", "Video Editing", "Graphic Design"]
+    
+    def get_rand(self):
+        return random.choice(self.hobbies_types)
+    
+
+class Friendship:
+    def __init__(self, name=None):
+        self.name = name
+        self.friendship_types = ["College Roommate Friends", "Childhood Playmates", "Work Colleagues Connection", "Sports Teammates Bond", "Neighborly Acquaintances", "Family Friends Since Birth", "Online Gaming Partners", "Book Club Companions", "Vacation Friends Met Abroad", "Mutual Hobby Enthusiasts", "Parent-Friends Through Kids", "High School Best Friends", "University Study Buddies", "Long Distance Pen Pals", "Ex-Partners Turned Friends", "Pet-Owner Friends Connection", "Fellow Musicians and Singers", "Travel Buddies From Trip", "Church Community Friends", "Local Social Club Members", "Military Service Friends", "Coworker Lunch Buddies", "Spouse's Friend Connection", "Therapy Group Comrades", "Yoga Class Companions", "Friends Through Siblings", "Old Friends Reconnected", "College Course Classmates", "Volunteering Companions", "Mentor-Mentee Relationship"]
+    
+    def get_rand(self):
+        return random.choice(self.friendship_types)
+    
+
+class Access:
+    def __init__(self, name=None):
+        self.name = name
+        self.access_types = ["Viewed Profile Only", "Left a Quick Note", "Added a Friendship", "Liked a Status Update", "Commented on Photo", "Checked Into Location", "Shared a Post to Timeline", "Uploaded New Profile Pic", "Joined a Public Group", "Participated in Poll", "Watched a Shared Video", "Responded to Event Invite", "Sent Direct Message", "Tagged in a Comment", "Started a Fundraiser", "Viewed Stories Archive", "Clicked on Marketplace Ad", "Muted Stories Temporarily", "Reacted to a Post", "Unfriended a Contact", "Reported an Issue", "Edited Profile Information", "Saved a Post for Later", "Requested Account Verification", "Listened to Shared Music", "Changed Privacy Settings", "Set Up Two-Factor Auth", "Subscribed to Notifications", "Completed a Quiz", "Unfollowed a Page"]
+    
+    def get_rand(self):
+        return random.choice(self.access_types)
+
+class MyTime:
+    def __init__(self):
+        self.current_time = int(time.time()) # current epoch time in seconds
+
+    def get_rand(self):
+        num_secs_in_year = 31536000
+        return random.randint(self.current_time - num_secs_in_year, self.current_time - num_secs_in_year)
+
+# # Test random generation
+
+# h = print(Hobby().get_rand())
+# f = print(Friendship().get_rand())
+# a = print(Access().get_rand())
+
+# mytime = MyTime().get_rand()
+
+# from datetime import datetime
+# dt_object = datetime.utcfromtimestamp(mytime)
+
+# print(dt_object)
